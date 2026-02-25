4e057b6a1b8bff35a1923aab2800e813bb2d6ee2
IvanLouren <ivanlourenco05@outlook.com>
2026-02-25 16:53:33 +0000

    database connectivity

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib" // pgx driver for database/sql
+)
+
+var DB *sql.DB
+
+func Connect() {
+	dsn := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatal("Failed to open database connection:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+
+	log.Println("Successfully connected to the database")
+	DB = db
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(0)
+}
