5a3ac61a81fc9d9b601e93115263116b3a7df9d8
IvanLouren <ivanlourenco05@outlook.com>
2026-03-01 18:08:30 +0000

    feat: Implement settlement creation and retrieval handlers

--- internal/settlements/handler.go
+++ internal/settlements/handler.go
@@ -1,1 +1,94 @@
 package settlements
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/IvanLouren/GoSplit/pkg/middleware"
+	"github.com/IvanLouren/GoSplit/pkg/models"
+	"github.com/google/uuid"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+type CreateSettlementRequest struct {
+	PaidTo string  `json:"paid_to"`
+	Amount float64 `json:"amount"`
+}
+
+func (h *Handler) CreateSettlement(w http.ResponseWriter, r *http.Request) {
+	userID := middleware.GetUserID(r)
+
+	parsedID, err := uuid.Parse(userID)
+	if err != nil {
+		http.Error(w, "invalid user ID in token", http.StatusUnauthorized)
+		return
+	}
+
+	groupIDStr := r.PathValue("id")
+	groupID, err := uuid.Parse(groupIDStr)
+	if err != nil {
+		http.Error(w, "invalid group ID", http.StatusBadRequest)
+		return
+	}
+
+	var req CreateSettlementRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if req.Amount <= 0 {
+		http.Error(w, "Amount must not be zero", http.StatusBadRequest)
+		return
+	}
+
+	if req.PaidTo == "" {
+		http.Error(w, "Paid To must not be empty(someone has to be assigned to receive the certain amount)", http.StatusBadRequest)
+		return
+	}
+
+	parsedPaidTo, err := uuid.Parse(req.PaidTo)
+	if err != nil {
+		http.Error(w, "invalid user ID in Paid To", http.StatusBadRequest)
+		return
+	}
+
+	settlement, err := h.service.CreateSettlement(groupID, parsedID, parsedPaidTo, req.Amount)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(settlement)
+}
+
+func (h *Handler) GetSettlements(w http.ResponseWriter, r *http.Request) {
+	groupIDStr := r.PathValue("id")
+	groupID, err := uuid.Parse(groupIDStr)
+	if err != nil {
+		http.Error(w, "invalid group ID", http.StatusBadRequest)
+		return
+	}
+
+	settlements, err := h.service.GetSettlements(groupID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if settlements == nil {
+		settlements = []models.Settlement{}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(settlements)
+}
--- internal/settlements/service.go
+++ internal/settlements/service.go
@@ -1,1 +1,48 @@
 package settlements
+
+import (
+	"database/sql"
+
+	"github.com/IvanLouren/GoSplit/pkg/models"
+	"github.com/google/uuid"
+)
+
+type Service struct {
+	db *sql.DB
+}
+
+func NewService(db *sql.DB) *Service {
+	return &Service{db: db}
+}
+
+func (s *Service) CreateSettlement(groupID, paidBy, paidTo uuid.UUID, amount float64) (models.Settlement, error) {
+	var settlement models.Settlement
+	err := s.db.QueryRow(`INSERT INTO settlements (group_id, paid_by, paid_to, amount) VALUES
+					 ($1, $2, $3, $4) RETURNING id, group_id, paid_by, paid_to, amount, created_at`, groupID, paidBy, paidTo, amount).
+		Scan(&settlement.ID, &settlement.GroupID, &settlement.PaidBy, &settlement.PaidTo, &settlement.Amount, &settlement.CreatedAt)
+	if err != nil {
+		return models.Settlement{}, err
+	}
+
+	return settlement, nil
+}
+
+func (s *Service) GetSettlements(groupID uuid.UUID) ([]models.Settlement, error) {
+	settlements, err := s.db.Query(`SELECT id, group_id, paid_by, paid_to, amount, created_at FROM settlements WHERE group_id = $1`, groupID)
+	if err != nil {
+		return nil, err
+	}
+	defer settlements.Close()
+
+	var result []models.Settlement
+	for settlements.Next() {
+		var settlement models.Settlement
+		err = settlements.Scan(&settlement.ID, &settlement.GroupID, &settlement.PaidBy, &settlement.PaidTo, &settlement.Amount, &settlement.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, settlement)
+	}
+
+	return result, nil
+}
